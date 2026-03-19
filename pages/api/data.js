0739d97c2032c274437d795ddfe58a91c80645c1
Steph4477 <morelstephane4026@neuf.fr>
2026-03-19 10:20:11 +0100

    feat: replace openweather api with open-meteo

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,8 +1,9 @@
+import config from "../../config.json";
+
 export default async function handler(req, res) {
-  const { cityInput } = req.body;
-  const getWeatherData = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`
+  const response = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${config.latitude}&longitude=${config.longitude}&current=temperature_2m,apparent_temperature,relative_humidity_2m,wind_speed_10m,wind_direction_10m&daily=sunrise,sunset`
   );
-  const data = await getWeatherData.json();
+  const data = await response.json();
   res.status(200).json(data);
 }
