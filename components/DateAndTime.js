e4d69bf14678b87e44fde04bd6edc821c57e1e53
Steph4477 <morelstephane4026@neuf.fr>
2026-03-19 14:14:17 +0100

    fix(ui): add day to date display

--- components/DateAndTime.js
+++ components/DateAndTime.js
@@ -2,10 +2,11 @@ import styles from "./DateAndTime.module.css";
 
 export const DateAndTime = ({ weatherData }) => {
   const date = new Date(weatherData.current.time);
+  const day = date.toLocaleDateString("fr-FR", { weekday: "long" });
 
   return (
     <div className={styles.wrapper}>
-      <h2>{date.getHours()}h</h2>
+      <h2>{day}, {date.getHours()}h</h2>
     </div>
   );
 };
